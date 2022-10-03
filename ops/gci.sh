1ce5b208df0fcc95af976541b16aa9522c1580ee
Gabor Javorszky <gabor@suborbital.dev>
2022-10-03 11:57:29 +0100

    Make lintfix run in exec form

--- ops/gci.sh
+++ ops/gci.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 gci write -s Standard -s Default -s "Prefix(github.com/suborbital)"  -s "Prefix(github.com/suborbital/framework-muxer-showdown)" --NoInlineComments --NoPrefixComments "$(find ./ -type f -name '*.go')"
