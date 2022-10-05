97659acbee47a12ae3a0ee74e27e68e0ee315cdb
Gabor Javorszky <gabor@suborbital.dev>
2022-10-05 18:26:56 +0100

    Implement health handler

--- app/app.go
+++ app/app.go
@@ -2,34 +2,59 @@ package app
 
 import (
 	"context"
+	stdLog "log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/rs/zerolog"
+	"github.com/suborbital/framework-muxer-showdown/errors"
+	"github.com/suborbital/framework-muxer-showdown/handlers"
 )
 
 type App struct {
 	logger  zerolog.Logger
 	errChan chan error
+	server  *http.Server
 }
 
 func New(l zerolog.Logger, errChan chan error) App {
 	handlerLogger := l.With().Str("module", "handlers").Logger()
 
+	stdLogs := stdLog.New(os.Stderr, "stdlogger", stdLog.Lmicroseconds)
+
+	mux := http.NewServeMux()
+	mux.Handle("/health", handlers.Health(handlerLogger))
+
 	return App{
-		logger:  l,
-		errChan: errChan,
+		logger: l,
+		server: &http.Server{
+			Addr:              "localhost:9000",
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       20 * time.Second,
+			ErrorLog:          stdLogs,
+		},
 	}
 }
 
 func (a App) Start() error {
 	a.logger.Info().Msg("started app")
 
+	if err := a.server.ListenAndServe(); err != nil {
+		return errors.NewShutdownError(err)
+	}
 	return nil
 }
 
 func (a App) Stop(reason string) {
-	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	a.logger.Info().Msgf("this is the server: %#v", a.server)
+
+	_ = a.server.Shutdown(ctx)
 	a.logger.Info().Msgf("stopped app for reason %s", reason)
 }
--- handlers/health.go
+++ handlers/health.go
@@ -1,3 +1,22 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/rs/zerolog"
+)
+
 const healthResponse = "everything working"
+
+// Health is going to be a health check implementation for net/http.
+func Health(logger zerolog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet && r.Method != http.MethodOptions {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		logger.Info().Msg("health handler called")
+
+		_, _ = w.Write([]byte(`{"message": "` + healthResponse + `"}`))
+	}
+}
