5b259915d39099af37445c5a879a2c943e53f181
Gabor Javorszky <gabor@suborbital.dev>
2022-10-12 16:26:39 +0100

    Do not check errors in req

--- handlers/errors_test.go
+++ handlers/errors_test.go
@@ -6,55 +6,61 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	localErrors "github.com/suborbital/framework-muxer-showdown/errors"
-	"github.com/suborbital/framework-muxer-showdown/web"
 )
 
 func TestErrorHandlers(t *testing.T) {
 	testlogger := zerolog.Nop()
 	testchan := make(chan error)
+	const path = "/error"
 
 	type args struct {
-		handler http.Handler
+		handler    http.Handler
+		middleware func(http.Handler) http.Handler
 	}
 	tests := []struct {
 		name       string
 		args       args
 		wantStatus int
 		wantBody   []byte
-		wantError  func(error) bool
 	}{
 		{
-			name:       "app error handler returns app error",
-			args:       args{handler: ErrorCatcher(testlogger, testchan)(ReturnsApplicationError(testlogger))},
+			name: "app error handler returns app error",
+			args: args{
+				handler:    ReturnsApplicationError(),
+				middleware: ErrorCatcher(testlogger, testchan),
+			},
 			wantStatus: http.StatusInternalServerError,
-			wantBody:   []byte(`{"Message":"app error: application error: some error from someplace"}`),
-			wantError:  localErrors.IsApplicationError,
+			wantBody:   []byte(`{"message":"app error: application error: some error from someplace"}`),
 		},
 		{
-			name:       "Not found error handler returns not found error",
-			args:       args{handler: ErrorCatcher(testlogger, testchan)(ReturnsNotFoundError())},
+			name: "Not found error handler returns not found error",
+			args: args{
+				handler:    ReturnsNotFoundError(),
+				middleware: ErrorCatcher(testlogger, testchan),
+			},
 			wantStatus: http.StatusNotFound,
-			wantBody:   []byte(`{"Message":"not found: not found: not found the thing"}`),
-			wantError:  localErrors.IsNotFoundError,
+			wantBody:   []byte(`{"message":"not found: not found: not found the thing"}`),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req := httptest.NewRequest(http.MethodGet, "/app-error", nil)
+			req := httptest.NewRequest(http.MethodGet, path, nil)
 			w := httptest.NewRecorder()
 
-			tt.args.handler.ServeHTTP(w, req)
+			r := chi.NewRouter()
+			r.With(tt.args.middleware).Get(path, tt.args.handler.ServeHTTP)
+
+			r.ServeHTTP(w, req)
 
 			gotBody, err := io.ReadAll(w.Result().Body)
 			require.NoError(t, err)
 
 			assert.Equal(t, tt.wantStatus, w.Result().StatusCode)
 			assert.Equalf(t, tt.wantBody, gotBody, "expected: %s\ngot: %s\n", tt.wantBody, gotBody)
-			assert.True(t, tt.wantError(web.GetErrors(req.Context())[0]))
 		})
 	}
 }
