32255bf74ea15a3c98d3ad6210efab572f72cb2d
Gabor Javorszky <gabor@suborbital.dev>
2022-10-12 16:12:49 +0100

    Add unit tests

--- go.mod
+++ go.mod
@@ -6,11 +6,15 @@ require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.28.0
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/net v0.0.0-20221004154528-8021a29435af
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ handlers/errors_test.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	localErrors "github.com/suborbital/framework-muxer-showdown/errors"
+	"github.com/suborbital/framework-muxer-showdown/web"
+)
+
+func TestErrorHandlers(t *testing.T) {
+	testlogger := zerolog.Nop()
+	testchan := make(chan error)
+
+	type args struct {
+		handler http.Handler
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantBody   []byte
+		wantError  func(error) bool
+	}{
+		{
+			name:       "app error handler returns app error",
+			args:       args{handler: ErrorCatcher(testlogger, testchan)(ReturnsApplicationError(testlogger))},
+			wantStatus: http.StatusInternalServerError,
+			wantBody:   []byte(`{"Message":"app error: application error: some error from someplace"}`),
+			wantError:  localErrors.IsApplicationError,
+		},
+		{
+			name:       "Not found error handler returns not found error",
+			args:       args{handler: ErrorCatcher(testlogger, testchan)(ReturnsNotFoundError())},
+			wantStatus: http.StatusNotFound,
+			wantBody:   []byte(`{"Message":"not found: not found: not found the thing"}`),
+			wantError:  localErrors.IsNotFoundError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/app-error", nil)
+			w := httptest.NewRecorder()
+
+			tt.args.handler.ServeHTTP(w, req)
+
+			gotBody, err := io.ReadAll(w.Result().Body)
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.wantStatus, w.Result().StatusCode)
+			assert.Equalf(t, tt.wantBody, gotBody, "expected: %s\ngot: %s\n", tt.wantBody, gotBody)
+			assert.True(t, tt.wantError(web.GetErrors(req.Context())[0]))
+		})
+	}
+}
