39ee3a94e11b472e9cb45e7c119e5126dfa8f2a6
Gabor Javorszky <gabor@suborbital.dev>
2022-10-13 21:25:34 +0100

    Ignore vendor directory in lintfix

--- ops/gci.sh
+++ ops/gci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gci write -s Standard -s Default -s "Prefix(github.com/suborbital)"  -s "Prefix(github.com/suborbital/framework-muxer-showdown)" --skip-generated --custom-order $(find . -type f -name '*.go')
+gci write -s Standard -s Default -s "Prefix(github.com/suborbital)"  -s "Prefix(github.com/suborbital/framework-muxer-showdown)" --skip-generated --custom-order $(find . -not \( -path ./vendor -prune \) -type f -name '*.go')
