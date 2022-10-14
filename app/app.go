f155cde209315a31640cfedbcd44f9166f2c9f96
Gabor Javorszky <gabor@suborbital.dev>
2022-10-14 13:17:23 +0100

    Add httprouter, and health endpoint

--- app/app.go
+++ app/app.go
@@ -2,34 +2,54 @@ package app
 
 import (
 	"context"
+	"net/http"
 	"time"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog"
+	"github.com/suborbital/framework-muxer-showdown/handlers"
 )
 
 type App struct {
 	logger  zerolog.Logger
 	errChan chan error
+	server  *http.Server
 }
 
 func New(l zerolog.Logger, errChan chan error) App {
 	handlerLogger := l.With().Str("module", "handlers").Logger()
 
+	r := httprouter.New()
+
+	r.GET("/health", handlers.Health(handlerLogger))
+
+	server := &http.Server{
+		Addr:              ":9000",
+		Handler:           r,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 1 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       20 * time.Second,
+	}
+
 	return App{
 		logger:  l,
 		errChan: errChan,
+		server:  server,
 	}
 }
 
 func (a App) Start() error {
 	a.logger.Info().Msg("started app")
 
-	return nil
+	return a.server.ListenAndServe()
 }
 
 func (a App) Stop(reason string) {
-	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	_ = a.server.Shutdown(ctx)
 
 	a.logger.Info().Msgf("stopped app for reason %s", reason)
 }
--- handlers/health.go
+++ handlers/health.go
@@ -1,3 +1,25 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog"
+)
+
 const healthResponse = "everything working"
+
+func Health(l zerolog.Logger) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		l.Info().Msg("health called")
+		enc, err := json.Marshal(messageResponse{Message: "everything working"})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write(nil)
+			return
+		}
+
+		_, _ = w.Write(enc)
+	}
+}
